30456726e7788b6bc5d1d919eb833ed40ee9d580
Tsubaki-01 <wengqiang9198@gmail.com>
2026-03-13 19:58:26 +0800

    「refactor」: Restructures database managers into sub-package

--- src/silver_pilot/dao/__init__.py
+++ src/silver_pilot/dao/__init__.py
@@ -1,5 +1,5 @@
-from .milvus_manager import MilvusManager
-from .neo4j_manager import Neo4jManager
+from .database.milvus_manager import MilvusManager
+from .database.neo4j_manager import Neo4jManager
 
 __all__: list[str] = [
     "MilvusManager",
---
+++ src/silver_pilot/dao/database/__init__.py
@@ -0,0 +1,7 @@
+from .milvus_manager import MilvusManager
+from .neo4j_manager import Neo4jManager
+
+__all__: list[str] = [
+    "MilvusManager",
+    "Neo4jManager",
+]
